2138f9a27358b5e7865db9c858e99d5c2d6e3d62
cgmuniz <cauamuniz00@gmail.com>
2022-09-05 02:07:13 -0300

    imagem padrão perfil e caixas de texto

--- lib/components/profile-edit/textfield_widget.dart
+++ lib/components/profile-edit/textfield_widget.dart
@@ -52,11 +52,6 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
           const SizedBox(height: 8),
           TextField(
             controller: controller,
-            decoration: InputDecoration(
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
-            ),
             maxLines: widget.maxLines,
             onChanged: widget.onChanged,
           ),
